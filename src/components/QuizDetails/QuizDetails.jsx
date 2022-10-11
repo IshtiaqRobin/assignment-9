6bfaf3323e91e93a41305e667aa3070224ac88e6
Ishtiaq <robin.pass36@gmail.com>
2022-10-11 18:25:11 +0600

    question style added

--- src/components/QuizDetails/QuizDetails.jsx
+++ src/components/QuizDetails/QuizDetails.jsx
@@ -1,10 +1,23 @@
+
+import { faCircleDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const QuizDetails = ({ topic }) => {
-    const { id, question, correctAnswer } = topic;
+    const { id, name, question, correctAnswer, options } = topic;
     return (
         <div>
-            <h3>Question: {question}</h3>
+            <p>{name}</p>
+            <div className='border-solid border-2 w-1/2 mx-auto'>
+                <h3>{question}</h3>
+                <div className='grid grid-cols-2 gap-4 w-1/2 mx-auto'>
+                    <div className='border-black border-solid border-2 rounded-sm px-1'><FontAwesomeIcon className='text-sky-400' icon={faCircleDot}></FontAwesomeIcon>{options[0]}</div>
+                    <div className='border-black border-solid border-2 rounded-sm px-1'><FontAwesomeIcon className='text-sky-400' icon={faCircleDot}></FontAwesomeIcon>{options[1]}</div>
+                    <div className='border-black border-solid border-2 rounded-sm px-1'><FontAwesomeIcon className='text-sky-400' icon={faCircleDot}></FontAwesomeIcon>{options[2]}</div>
+                    <div className='border-black border-solid border-2 rounded-sm px-1'><FontAwesomeIcon className='text-sky-400' icon={faCircleDot}></FontAwesomeIcon>{options[3]}</div>
+                </div>
+            </div>
+
         </div>
     );
 };
