c9c6bde96771cc4a7ee56b0331f71c012493f98c
Ishtiaq <robin.pass36@gmail.com>
2022-10-12 08:46:44 +0600

    header link design complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <div className='header w-full bg-slate-100 mb-12'>
             <div className='flex sm:justify-around md:w-4/5 lg:justify-between mx-auto lg:w-4/5 py-6'>
                 <div className=''>
-                    <Link to='/home'><p className='text-5xl text-sky-600 font-semibold'>Quizizz</p></Link>
+                    <Link to='/home'><img src="https://assets-global.website-files.com/60aca2b71ab9a5e4ececf1cf/62fa6419161d3a641f681ceb_Logo.svg" alt="" /></Link>
                 </div>
                 <div className='header-link'>
                     <nav className='text-lg text-sky-600 font-semibold'>
@@ -17,7 +17,6 @@ const Header = () => {
                     </nav>
                 </div>
             </div>
-
         </div>
     );
 };
